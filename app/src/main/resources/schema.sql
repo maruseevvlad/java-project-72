a8d23a060d0fe007c63c11e3bb9eebf613b4ad87
maruseevvlad <maruseev.vlad@mail.ru>
2025-09-07 20:00:22 +0300

    schema.sql

---
+++ app/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS courses;
+
+//url
+--CREATE TABLE courses (
+--    id INT PRIMARY KEY AUTO_INCREMENT,
+--    name VARCHAR(255) NOT NULL,
+--    description TEXT,
+--    created_at TIMESTAMP
+--);
